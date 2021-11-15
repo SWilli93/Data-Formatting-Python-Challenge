d7d7e4480167ac89a9144bbb7c450c9fff39e229
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-15 14:27:32 -0500

    strip and replace elements

--- Data_Formatting_Challenge.py
+++ Data_Formatting_Challenge.py
@@ -115,3 +115,12 @@ green&white;,;09/15/17,   Gail Phelps   ;,;$30.52
 # Unfortunately, the Thread Shed has an extremely outdated register system and stores all of the transaction information in one huge unwieldy string called daily_sales.
 
 # All day, for each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. Your task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists.
+
+
+daily_sales_replaced = daily_sales.replace(";,;", " bob ")
+# print(daily_sales_replaced)
+new_daily_sales_replaced = daily_sales_replaced.strip('\n')
+# print(new_daily_sales_replaced)
+
+daily_transactions = new_daily_sales_replaced.split(",")
+# print(daily_transactions)
