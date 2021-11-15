4bf79155eddc86928293b24dda7b05b5ade6f662
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-15 14:00:25 -0500

    Init

--- Data_Formatting_Challenge.py
+++ Data_Formatting_Challenge.py
@@ -114,4 +114,4 @@ green&white;,;09/15/17,   Gail Phelps   ;,;$30.52
 
 # Unfortunately, the Thread Shed has an extremely outdated register system and stores all of the transaction information in one huge unwieldy string called daily_sales.
 
-# All day, for each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. Your task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists.
+# All day, for each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. Your task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists. 
